fc6f3d36859d00843372085c53533efc18cd6e8b
MaciejkaG <me@mcjk.cc>
2024-08-01 17:31:39 +0200

    Update service-worker.js

--- public/assets/js/service-worker.js
+++ public/assets/js/service-worker.js
@@ -1,9 +1,10 @@
-const statki = "statki-by-maciejka-0.7.6";
+const statki = "statki-by-maciejka-0.10.1";
 const assets = [
     "/favicon.ico",
     "/assets/css/landing.css",
     "/assets/css/main.css",
     "/assets/css/responsive.css",
+    "/assets/css/match.css",
     "/assets/css/board.css",
     "/assets/css/board-responsive.css",
     "/assets/js/battleships-lib.js",
@@ -13,7 +14,9 @@ const assets = [
     "/assets/js/socket-err-handler.js",
     "/assets/js/socket-game.js",
     "/assets/js/socket.js",
-    "/assets/js/spa_lib.js"
+    "/assets/js/spa_lib.js",
+    "/assets/img/check-circle.svg",
+    "/assets/img/shopping-cart.svg"
 ];
 
 self.addEventListener("install", installEvent => {
